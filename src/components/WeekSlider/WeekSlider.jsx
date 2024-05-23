89ae9172795330eb81ab88cb6ee467f2f57a8409
kubat <kubat.aiupov@gmail.com>
2024-05-23 13:02:43 +0200

    add slider

--- src/components/WeekSlider/WeekSlider.jsx
+++ src/components/WeekSlider/WeekSlider.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
-import styles from "./WeekSlider.module.css";
+import "./WeekSlider.css";
+import ReactSlider from "react-slider";
 
 const WeekSlider = () => {
   const [toggleWeek, setToggleWeek] = useState(true);
+  const [value, setValue] = useState([]);
 
   const handleToggleWeek = (e) => setToggleWeek(e.target.checked);
   return (
-    <main>
+    <main className="week-container">
       <form>
         <input
           type="checkbox"
@@ -14,6 +16,25 @@ const WeekSlider = () => {
           onChange={handleToggleWeek}
         />
       </form>
+      {toggleWeek && (
+        <ReactSlider
+          className="horizontal-slider"
+          thumbClassName="example-thumb"
+          trackClassName="example-track"
+          defaultValue={[0, 6]}
+          ariaLabel={["Leftmost thumb", "Middle thumb", 'Rightmost thumb']}
+          ariaValuetext={(state) => `Thumb value ${state.valueNow}`}
+          renderThumb={(props, state) => <div {...props}>{state.valueNow}</div>}
+          pearling
+          marks
+          min={0}
+          max={6}
+          minDistance={1}
+          onChange={(v) => setValue(v)}
+        />
+      )}
+      <br />
+      {value}
     </main>
   );
 };
